a0b1109b1d915ef328169e7909b4ff8a99429bfc
Rastislav Pečík <rapecik@gmail.com>
2024-09-20 17:28:54 +0200

    added persistent keycloak deployment

--- keycloak/process.sh
+++ keycloak/process.sh
@@ -1,5 +1,15 @@
+oc process -f postgresql-persistent.yaml \
+    -p POSTGRESQL_USER=keycloak \
+    -p POSTGRESQL_PASSWORD=keycloak \
+    -p NAMESPACE=openshift \
+    -p POSTGRESQL_DATABASE=keycloak \
+| oc apply -f - -n ai-roadshow
+
+
 oc process -f keycloak.yaml \
     -p KEYCLOAK_ADMIN=admin \
     -p KEYCLOAK_ADMIN_PASSWORD=admin \
+    -p KEYCLOAK_DB_PASSWORD=keycloak \
+    -p KEYCLOAK_DB_USERNAME=keycloak \
     -p NAMESPACE=keycloak \
-| oc apply -f - -n keycloak
+| oc apply -f - -n ai-roadshow
